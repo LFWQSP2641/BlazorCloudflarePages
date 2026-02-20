3be2061aaffd332fd9eb7926fa9937326f2f4bc2
LFWQSP2641 <liuyunbo2641@gmail.com>
2026-02-20 11:22:45 +0800

    Add build.sh

---
+++ Scripts/build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
+chmod +x dotnet-install.sh
+./dotnet-install.sh -c 10.0 -InstallDir ./dotnet10
+./dotnet10/dotnet --version
+./dotnet10/dotnet publish -c Release -o output
